b96e72ef1431dfb30768d4e9f0c954d5f5b53569
Eldon Yoder <eldonyoder@gmail.com>
2014-04-30 07:47:01 -0400

    basic crud on events

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+group :development do
+  gem "rails-erd"
+end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,51 @@
 class EventsController < ApplicationController
+
+	def index
+		@all_events = Event.all
+	end
+
+	def new
+		@new_event = Event.new
+	end
+
+	def create
+		@new_event = Event.new(event_params)
+		if @new_event.save
+			redirect_to events_path
+		else
+			redirect_to new_event_path
+		end
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		if @event.update_attributes(event_params)
+			redirect_to events_path
+		else
+			redirect_to edit_event_path
+		end
+	end
+
+	def destroy
+		@event = Event.find(params[:id])
+		if @event.delete
+			redirect_to events_path
+		else
+			redirect_to event_path
+		end
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:name, :description, :contact_name, :contact_email, :contact_phone, :event_start_date, :event_end_date, :expire_on, :post_id)
+	end
 end
